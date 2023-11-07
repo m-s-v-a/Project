50798088b30442b0dd7886df6a4f0494a7042684
m-s-v-a <moseeva.dasha37@gmail.com>
2023-11-07 09:00:28 +0300

    commit2

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const path = require('node:path')
+const fs = require('node:fs')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+app.use(express.static('static'));
+app.use(express.urlencoded())
+
+app.get('*', (req, res) => {
+    res.send()
+})
+app.get('/registration', (req, res) => {
+    res.sendFile("index.html",{root: path.join(__dirname, "./static")})
+})
+app.post('/reg-data',(req, res)=>{
+    console.log(">>>>>>>>>>", req.body)
+    fs.appendFile("data.txt", JSON.stringify(req.body) + "\n", (err)=>{
+        if(err){
+            res.status(500).send("User not added")
+        }
+        else {
+            res.status(201).send("User added")
+        }
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
